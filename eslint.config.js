860a83b697cb8218935aaebb2df3197b43230be8
Patryk Jankowski <p.jankowski187@gmail.com>
2025-07-05 13:20:49 +0200

    fix: poprawka konfiguracji Eslint pod Jest i ESM

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,35 @@
 import js from "@eslint/js";
 import globals from "globals";
+import jestPlugin from "eslint-plugin-jest";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  // Podstawowe lintowanie JS wg recommended
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module", // dla import/export
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
+  // Reguły specyficzne dla testów z Jest
+  {
+    files: ["**/*.test.js"],
+    plugins: { jest: jestPlugin },
+    extends: ["plugin:jest/recommended"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest, // rozpoznaje describe/test/expect
+      },
+    },
+  },
 ]);
